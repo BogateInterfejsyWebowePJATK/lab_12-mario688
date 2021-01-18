cf938f9f9749749dc76f26a055b8ff6799aa6eb6
mario688 <gliwamario@gmail.com>
2021-01-18 20:34:32 +0100

    zad 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 
 export default function Form() {
-    const [number, setNumber] = useState(0);
-    const [sum, setSum] = useState(0);
-    const [NumberOfnumbers, setNumberOfnumbers] = useState(0);
+    const [number, setNumber] = useState("");
+    
     
     const submit = e => {
         e.preventDefault();
@@ -13,17 +12,17 @@ export default function Form() {
     };
     const average=sum/NumberOfnumbers;
     return (
-        <div className="CalcForm">
+        <>
             <h1>Sum:{sum} Average:{average}</h1>
             
             <form onSubmit={submit}>
                     <input value={number} onChange={e => setNumber(e.target.value)}
                     type="text"
-                    placeholder="Input number"
+                    placeholder="Insert number"
                     required
                 />
-                <button>ADD</button>
+                <button>Calculate</button>
             </form>
-        </div>
+        </>
     );
 }
