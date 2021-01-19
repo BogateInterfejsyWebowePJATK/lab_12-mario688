524f8b8ab4f1acbfc6d33f9ce1d8a0197ff7cfb1
mario688 <gliwamario@gmail.com>
2021-01-19 20:19:29 +0100

    Zad3

---
+++ src/components/Form.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import User from '../data/User.json'
+class Form extends React.Component {
+    constructor() {
+    super();
+    this.state = {
+      input: {},
+      errors: {}
+    };
+     
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+    
+     
+  handleChange(event) {
+    let input = this.state.input;
+    input[event.target.name] = event.target.value;
+  
+    this.setState({
+      input
+    });
+  }
+    
+  handleSubmit(event) {
+    event.preventDefault();
+  
+    if(this.validate()){
+        console.log(this.state);
+  
+        let input = {};
+        input["name"] = "";
+        input["email"] = "";
+        var correct=false;
+        this.setState({input:input});
+        {User.map((User, index)=>{
+          if(document.getElementById('name').value==User.password && document.getElementById('email').value==User.email ){
+            correct=true;
+          }else 
+            alert("incorrect email or password!")
+           
+         
+             
+      })}
+      if(correct)
+        alert('Zalogowany!');
+      
+
+    }
+  }
+  
+  validate(){
+      let input = this.state.input;
+      let errors = {};
+      let isValid = true;
+  
+      if (!input["name"]) {
+        isValid = false;
+        errors["name"] = "Please enter your name.";
+      }
+  
+      if (!input["email"]) {
+        isValid = false;
+        errors["email"] = "Please enter your email Address.";
+      }
+  
+      if (typeof input["email"] !== "undefined") {
+          
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        if (!pattern.test(input["email"])) {
+          isValid = false;
+          errors["email"] = "Please enter valid email address.";
+        }
+      }
+      
+     
+  
+      this.setState({
+        errors: errors
+      });
+  
+      return isValid;
+  }
+     
+  render() {
+    return (
+      <div>
+        
+        <form onSubmit={this.handleSubmit}>
+  
+          <div class="form-group">
+            <label for="name">Password:</label>
+            <input 
+              type="password" 
+              name="name" 
+              value={this.state.input.name}
+              onChange={this.handleChange}
+              class="form-control" 
+              placeholder="Enter name" 
+              id="name" />
+
+              <div className="text-danger">{this.state.errors.name}</div>
+          </div>
+  
+          <div class="form-group">
+            <label for="email">Email Address:</label>
+            <input 
+              type="text" 
+              name="email" 
+              value={this.state.input.email}
+              onChange={this.handleChange}
+              class="form-control" 
+              placeholder="Enter email" 
+              id="email" />
+   
+              <div className="text-danger">{this.state.errors.email}</div>
+          </div>
+          <input type="submit" value="Zaloguj" class="btn btn-success" />
+        </form>
+      </div>
+
+    );
+  }
+}
+  
+export default Form;
